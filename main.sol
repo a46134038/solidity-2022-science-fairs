b3372da2add0439fc6daf9e0373edd1afe874b62
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-13 11:01:48 +0800

    Add files via upload

---
+++ main.sol
@@ -0,0 +1,26 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+import "token/token.sol";
+
+
+contract bank is token {
+
+
+    uint a;
+
+
+    constructor() {
+        a = 15;
+
+
+    }
+
+
+    function print() public view returns(uint) {
+        return a;
+    }
+
+
+}
---
+++ token/token.sol
@@ -0,0 +1,47 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+contract token {
+
+
+    struct TransferHistory{
+        address sender;
+        address receiver;
+        uint amount;
+    }
+
+
+    mapping (address => uint) balance;
+    mapping (address => TransferHistory[]) transferHistory;
+
+
+    function changeBalance(address addr,uint amount) public {
+        balance[addr] = amount;
+    }
+
+
+    function transfer(address addr,uint160 amount) public {
+        require(balance[msg.sender] >= amount,"Your balance is not enough");
+        balance[msg.sender] -= amount;
+        balance[addr] += amount;
+        transferHistory[msg.sender].push(TransferHistory(msg.sender,addr,amount));
+    }
+
+
+  /*  function showLastTransferHistory(uint amount = 1) public view returns() {
+
+
+    }
+*/
+    function showAllTransferHistory() public view returns(TransferHistory[]) {
+        return transferHistory[msg.sender];
+    }
+
+
+    function showBalance() public view returns(uint) {
+        return balance[msg.sender];
+    }
+
+
+}
