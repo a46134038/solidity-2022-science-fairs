29d4f34350e879b0df0be56752c67e22f9eae42a
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-17 15:01:33 +0800

    Update token.sol

--- token/token.sol
+++ token/token.sol
@@ -6,6 +6,7 @@ contract token {
 
 
     struct TransferHistory{
+        string transferType;
         address sender;
         address receiver;
         uint amount;
@@ -16,27 +17,32 @@ contract token {
 
     function setBalance(address addr,uint amount) public {
         balance[addr] = amount;
-        transferHistory[addr].push(TransferHistory(address(this),addr,amount));
+        transferHistory[addr].push(TransferHistory("setBalance",address(this),addr,amount));
     }
 
     function changeBalance(address addr,int amount) public {
         int new_balance = int(balance[addr]) + amount;
         require(new_balance >= 0 ,"Your balance is not enough");
         balance[addr] = uint(new_balance);
-        transferHistory[addr].push(TransferHistory(address(this),addr,amount));
+
+        if(amount < 0) {
+            transferHistory[addr].push(TransferHistory("deduction",address(0),addr,uint(-amount)));
+        }else {
+            transferHistory[addr].push(TransferHistory("induction",address(0),addr,uint(amount)));
+        }
     }
 
     function transfer(address addr,uint160 amount) public {
         require(balance[msg.sender] >= amount,"Your balance is not enough");
         balance[msg.sender] -= amount;
         balance[addr] += amount;
-        transferHistory[msg.sender].push(TransferHistory(msg.sender,addr,amount));
-        transferHistory[addr].push(TransferHistory(msg.sender,addr,amount));
+        transferHistory[msg.sender].push(TransferHistory("transfer",msg.sender,addr,amount));
+        transferHistory[addr]a.push(TransferHistory("transfer",msg.sender,addr,amount));
     }
-  /*
-    function showLastTransferHistory(uint amount) public view returns() {
-
-    }*/
+  
+    function showLastTransferHistory() public view returns(TransferHistory[] memory) {
+        return transferHistory[msg.sender];
+    }
 
     function showAllTransferHistory() public view returns(TransferHistory[] memory) {
         return transferHistory[msg.sender];
