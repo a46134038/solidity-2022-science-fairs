56e254612bff38b9de2fc50d17c74587d5bf89e3
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-17 13:28:03 +0800

    Update token.sol

--- token/token.sol
+++ token/token.sol
@@ -16,10 +16,14 @@ contract token {
 
     function setBalance(address addr,uint amount) public {
         balance[addr] = amount;
+        transferHistory[addr].push(TransferHistory(address(this),addr,amount));
     }
 
     function changeBalance(address addr,int amount) public {
-        balance[addr] += amount;
+        int new_balance = int(balance[addr]) + amount;
+        require(new_balance >= 0 ,"Your balance is not enough");
+        balance[addr] = uint(new_balance);
+        transferHistory[addr].push(TransferHistory(address(this),addr,amount));
     }
 
     function transfer(address addr,uint160 amount) public {
@@ -27,22 +31,24 @@ contract token {
         balance[msg.sender] -= amount;
         balance[addr] += amount;
         transferHistory[msg.sender].push(TransferHistory(msg.sender,addr,amount));
+        transferHistory[addr].push(TransferHistory(msg.sender,addr,amount));
     }
+  /*
+    function showLastTransferHistory(uint amount) public view returns() {
 
+    }*/
 
-/*  
-    function showLastTransferHistory(uint amount = 1) public view returns() {
-
-
-    }
-*/
-    function showAllTransferHistory() public view returns(TransferHistory[]) {
+    function showAllTransferHistory() public view returns(TransferHistory[] memory) {
         return transferHistory[msg.sender];
     }
 
 
     function showBalance() public view returns(uint) {
         return balance[msg.sender];
+    }
+
+    function getContractAddress() public view returns(address) {
+        return address(this);
     }
 
 }
