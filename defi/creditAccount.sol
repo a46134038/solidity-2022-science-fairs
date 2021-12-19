1e0bc86a8ca6f53fadfd9ed2d74dcd34fe530de5
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-19 22:57:55 +0800

    Update creditAccount.sol

--- defi/creditAccount.sol
+++ defi/creditAccount.sol
@@ -4,18 +4,29 @@ pragma solidity ^0.8.7;
 contract creditAccount {
 
     mapping (address => int) liability;
+    
+    uint stakePool;
 
     function lend(uint amount) public {
-
         liability[msg.sender] -= int(amount); 
+        stakePool -= amount;
     }
 
     function payDebt(uint amount) public {
         liability[msg.sender] += int(amount);
+        stakePool += amount;
     }
 
     function showAccountEquity() public view returns(int) {
         return liability[msg.sender];
+    }
+
+    function getPoolSize() public view returns(uint) {
+        return stakePool;
+    }
+
+    function getInterestRate() public view returns(uint) {
+        
     }
 
 }
