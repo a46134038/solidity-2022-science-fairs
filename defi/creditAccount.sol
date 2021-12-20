15ea7acea9a46400f375a3763747eda07ac4a049
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-20 11:55:27 +0800

    Update creditAccount.sol

--- defi/creditAccount.sol
+++ defi/creditAccount.sol
@@ -9,8 +9,11 @@ contract creditAccount {
     }
 
     struct Liability {
+        uint balance;
         uint lendTotal;
         uint stakeTotal;
+        uint interestRevenue;
+        uint interestExpense;
     }
     
     mapping (address => Liability) liability; 
@@ -18,24 +21,32 @@ contract creditAccount {
     StakePool stakePool;
 
     function stake(uint amount) public { // 存入現金賺取利息
-        require("");
+        require(liability[msg.sender].balance >= amount,"Your balance is not enough");
         liability[msg.sender].stakeTotal += amount;
         stakePool.poolStakeTotal += amount;
     }
 
     function redeem(uint amount) public { // 取消存款
+        require(liability[msg.sender].stakeTotal >= amount,"Your deposit balance is not enough");
         liability[msg.sender].stakeTotal -= amount;
         stakePool.poolStakeTotal -= amount;
+        liability[msg.sender].balance += (amount + liability[msg.sender].interestRevenue);
+        liability[msg.sender].interestRevenue = 0;
     }
 
     function lend(uint amount) public { // 借款
+        require(stakePool.poolStakeTotal >= amount,"Stake pool balance is not enough");
         liability[msg.sender].lendTotal += amount;
         stakePool.poolLendTotal += amount;
     }
 
     function payDebt(uint amount) public { // 還款
+        require(liability[msg.sender].lendTotal >= amount,"Your repayment amount is more then lend amount");
+        require(liability[msg.sender].balance >= amount,"Your balance is not enough");
         liability[msg.sender].lendTotal -= amount;
         stakePool.poolLendTotal -= amount;
+        liability[msg.sender].balance -= (amount + liability[msg.sender].interestExpense);
+        liability[msg.sender].interestExpense = 0;
     }
 
     function showAccountDebtTotal() public view returns(uint) { // 顯示帳戶負債
