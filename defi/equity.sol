33d25e2fd36b6951d46df8dd1bff2fdd7d44127f
a46134038 <47667665+a46134038@users.noreply.github.com>
2021-12-28 15:56:25 +0800

    Update equity.sol

--- defi/equity.sol
+++ defi/equity.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 contract equity {
     
-    struct TransferHistory{
+    struct EquityTransferHistory {
         string transferType;
         address sender;
         address receiver;
@@ -11,7 +11,7 @@ contract equity {
     }
     
     mapping (address => uint) shares;
-    mapping (address => TransferHistory[]) transferHistory;
+    mapping (address => EquityTransferHistory[]) private transferHistory;
 
     uint supply = 0;
 
@@ -19,36 +19,36 @@ contract equity {
         require(shares[msg.sender]>=amount,"Your shares is not enough");
         shares[msg.sender] -= amount;
         shares[addr] += amount;
-        transferHistory[msg.sender].push(TransferHistory("Transfer",msg.sender,addr,amount));
-        transferHistory[addr].push(TransferHistory("Transfer",msg.sender,addr,amount));
+        transferHistory[msg.sender].push(EquityTransferHistory("Transfer",msg.sender,addr,amount));
+        transferHistory[addr].push(EquityTransferHistory("Transfer",msg.sender,addr,amount));
     }
 
     function equityMint(address addr,uint amount) public { // 股權鑄造
         shares[addr] += amount;
-        supply += amount;
-        transferHistory[addr].push(TransferHistory("Mint",address(0),addr,amount));
+        supply += amount;a
+        transferHistory[addr].push(EquityTransferHistory("Mint",address(0),addr,amount));
     }
 
     function equityDestory(address addr,uint amount) public { // 股權銷毀
         require(shares[addr]>=amount,"Your shares is not enough");
         shares[addr] -= amount;
         supply -= amount;
-        transferHistory[addr].push(TransferHistory("Destory",address(0),addr,amount));
+        transferHistory[addr].push(EquityTransferHistory("Destory",address(0),addr,amount));
     }
 
-    function equityShowLastTransferHistory(uint amount) public view returns(TransferHistory[] memory) { // 查詢最後n筆交易
+    function equityShowLastTransferHistory(uint amount) public view returns(EquityTransferHistory[] memory) { // 查詢最後n筆交易
         if(amount > transferHistory[msg.sender].length) {
             amount = transferHistory[msg.sender].length;
         }
 
-        TransferHistory[] memory data = new TransferHistory[](amount);
+        EquityTransferHistory[] memory data = new EquityTransferHistory[](amount);
         for(uint i = amount ; i > 0 ; i--) {
             data[amount-i] = transferHistory[msg.sender][transferHistory[msg.sender].length-(amount-i+1)];
         }
         return data;
     }
 
-    function equityShowAllTransferHistory() public view returns(TransferHistory[] memory) { // 查詢所有交易
+    function equityShowAllTransferHistory() public view returns(EquityTransferHistory[] memory) { // 查詢所有交易
         return transferHistory[msg.sender];
     }
 
