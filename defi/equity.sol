68f0b396e79eb58e2fcb7283b09b1880a7036a2a
a46134038 <47667665+a46134038@users.noreply.github.com>
2022-01-06 15:17:12 +0800

    Update equity.sol

--- defi/equity.sol
+++ defi/equity.sol
@@ -3,17 +3,32 @@ pragma solidity ^0.8.7;
 
 contract equity {
     
+    struct Proposal {
+        string proposalName;
+        string proposalInfo;
+        uint disagreeCount;
+        uint agreeCount;
+        uint proposalTime;
+        uint proposalDeadline;
+    }
+
+    struct ProposeHistory {
+        Proposal proposal;
+    }
+
     struct EquityTransferHistory {
         string transferType;
         address sender;
         address receiver;
         uint amount;
     }
-    
+
     mapping (address => uint) shares;
     mapping (address => EquityTransferHistory[]) private transferHistory;
+    mapping (address => uint) lastProposeTimeStamp;
 
     uint supply = 0;
+    Proposal[] proposal;
 
     function equityTransfer(address addr,uint amount) public { // 股權過戶
         require(shares[msg.sender]>=amount,"Your shares is not enough");
@@ -25,7 +40,7 @@ contract equity {
 
     function equityMint(address addr,uint amount) public { // 股權鑄造
         shares[addr] += amount;
-        supply += amount;a
+        supply += amount;
         transferHistory[addr].push(EquityTransferHistory("Mint",address(0),addr,amount));
     }
 
@@ -35,6 +50,33 @@ contract equity {
         supply -= amount;
         transferHistory[addr].push(EquityTransferHistory("Destory",address(0),addr,amount));
     }
+
+    function propose(string memory proposalName,string memory proposalInfo) public {
+        require(lastProposeTimeStamp[msg.sender] > block.timestamp-(block.timestamp%86400)+(86400*7),"You have already proposed , Please wait for 7 days to propose again");
+
+        proposal.push(Proposal(proposalName,proposalInfo,0,0,block.timestamp,block.timestamp-(block.timestamp%86400)+(86400*30)));
+        lastProposeTimeStamp[msg.sender] = block.timestamp;
+    }
+
+    function showAllActiveProposal() view public returns(Proposal[] memory) {
+
+        Proposal[] memory data = new Proposal[](proposal.length);
+        uint amount = proposal.length;
+        while(amount-->0){
+            if(proposal[amount].proposalDeadline > block.timestamp) {
+                data.push(proposal[amount]);
+            }
+        }
+        return data;
+    }
+
+    function showAllProposal() public view returns(Proposal[] memory) {
+        return proposal;
+    }
+
+    /*function vote() public {
+
+    }*/
 
     function equityShowLastTransferHistory(uint amount) public view returns(EquityTransferHistory[] memory) { // 查詢最後n筆交易
         if(amount > transferHistory[msg.sender].length) {
